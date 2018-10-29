7ccaf33c03f61fcbdc3abc5560d0858d1fc73500
Alex C <tyxou@hotmail.com>
2018-10-29 18:22:17 +0100

    properties aka props

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-// a simple stateless component
-function Trello() {
-  return (
-    <div className="note">
-      <h2>Note</h2>
-      <h3>Edit/Remove</h3>
-    </div>
-  );
+// let's start by "translating" the below component
+// which now we've commented-out
+// function Trello() {
+//   return (
+//     <div className="note">
+//       <h2>Note</h2>
+//       <h3>Edit/Remove</h3>
+//     </div>
+//   );
+// }
+
+class Trello extends React.Component {
+
+  render() {
+    return (
+      <div className="note">
+        <h2>{this.props.title} from {this.props.user}</h2>
+        <h3>Edit/Remove</h3> 
+      </div>
+    )
+  }
 }
 
-// for this component to become visible on the DOM,
-// we must instruct React's DOM to do so:
-ReactDOM.render(<Trello />, document.getElementById('content'));
+ReactDOM.render( 
+  <Trello title="Note" user="Joe" />,
+  document.getElementById('content') )
 
-// we can see that the ReactDOM render method takes 2 arguments:
-// the 1st argument is the component/s to render, and the 2nd arg
-// is to tell the DOM where it should render this component.
-
-// MULTIPLE COMPONENTS:
-// as will be discussed, React does NOT like adjacent components,
-// but it can be easily dealt with by wrapping any components needed
-// inside a "wrapper" - a <div> wrap
-
-// let's comment-out line 13 above, in which we are calling
-// the ReactDOM to render our Trello component,
-// and then un-comment the lines below to call it:
+// after comment-out the abouve ReactDOM,
+// uncomment the below and look at the browser.
 
 // ReactDOM.render(
 //   <div>
-//     <Trello />
-//     <Trello />
+//     <Trello title="Note" user="Joe" />
+//     <Trello title="Comment" user="Neil" />
 //   </div>,
-//   document.getElementById('content')
-// );
+//   document.getElementById('content') )
