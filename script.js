7a7703af332564e9ba5a47d46cdd609c4c9b030f
Alex C <tyxou@hotmail.com>
2018-10-30 01:35:27 +0100

    events

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 class Trello extends React.Component {
 
+  clicker() {
+    console.log(`This displays children passed: \n ${this.props.children}`);
+    // for the above we need to open up the browser's Dev Tools => JS Console
+    // the below shows up as alert, in case you cannot find the JS Console
+    alert(this.props.children);
+  }
+
   render() {
     return (
       <div className="note">
         <h2>{this.props.title} from {this.props.user}</h2>
-        <p>{this.props.children}</p>
+        <p onClick={()=>this.clicker()}>{this.props.children}</p>
       </div>
     )
   }
 }
 
-// so far we've always passed multiple components
-// directly to ReactDOM, and although functional,
-// Good Programming Practices for React dictates
-// creating a Layout component to hold the needed components
 
 function Layout(props) {
   return (
     <div>
 
       <Trello title="Note" user="John">
-        <button className="btn-info">Edit / Save</button>
+        <button className="btn-info">Edit / Save</button>  {/* <= anything here is children */}
       </Trello>
 
       <Trello title="Comment" user="Jane">
-        children as dud-button text
+        children as dud-button text {/* <= anything here is children */}
       </Trello>
 
     </div>
   )
 }
+
+// You can note that in the case of the button, when it is passed
+// as children, the console.log() and alert() show it up as [object Object]
+// this is normal behaviour for some browsers. It is very difficult to log it out but possible.
 
 const content = document.getElementById('content');
 
